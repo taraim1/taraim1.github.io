9361fb9437349c23ce59a4d17484da8a5bc702dc
taraim <kimjeyul200426@gmail.com>
2025-08-17 07:01:23 +0900

    migrate project to Node.js

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # portfolio-page
 김제율의 포트폴리오 페이지입니다.
 
-HTML/CSS/Javascript 학습 및 연습용으로 만들었습니다.
+HTML/CSS/Javascript/Node.js 학습 및 연습용으로 만들었습니다.
---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", function (req, res) {
+  const ProjectsFilePath = path.join(__dirname, "data", "projects.json");
+
+  const ProjectsFileData = fs.readFileSync(ProjectsFilePath);
+  const storedProjects = JSON.parse(ProjectsFileData);
+
+  const SkillsFilePath = path.join(__dirname, "data", "skills.json");
+
+  const SkillsFileData = fs.readFileSync(SkillsFilePath);
+  const storedSkills = JSON.parse(SkillsFileData);
+
+
+  res.render("index", { projects: storedProjects, skills: storedSkills });
+});
+
+app.listen(443);
